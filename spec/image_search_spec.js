3bdc227880d382a72432771f624860af2cf36c62
Alex Flores <eflores@mdsol.com>
2015-12-19 23:42:00 -0500

    add test harness

---
+++ spec/image_search_spec.js
@@ -0,0 +1,17 @@
+const imageSearch = require('../dist/lib/image_search')
+const chai        = require('chai')
+const expect      = chai.expect
+const sinon       = require("sinon")
+const sinonChai   = require("sinon-chai")
+chai.use(sinonChai)
+
+
+describe("imageSearch", function() {
+  
+  xit("fetches an image url", function() {
+  })
+  
+  xit("returns when encountering an error", function() {
+  })
+  
+})
